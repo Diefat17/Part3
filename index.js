8dc7843c0a8b0a9eab89505450dc4df6a32b29e9
Diegordo17 <diegosolorzano608@gmail.com>
2024-04-28 12:11:41 -0600

    Deploy backend

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
+app.use(cors())
+app.use(express.json())
 
 
 let notes = [
@@ -21,7 +24,6 @@ let notes = [
   }
 ]
 
-app.use(express.json())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -76,7 +78,7 @@ app.delete('/api/notes/:id', (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
