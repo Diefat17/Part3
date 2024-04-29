a025418cf7a96219a429f2aac08b2efe3cfe655f
Diegordo17 <diegosolorzano608@gmail.com>
2024-04-28 18:54:14 -0600

    Trying to work

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ let notes = [
 
 
 app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+  response.send('/dist/index.html')
 })
 
 app.get('/api/notes', (request, response) => {
