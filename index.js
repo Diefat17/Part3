ff312520692ff39021a44b5e0ebf5d266c4761fa
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-05 15:01:41 -0600

    Add static(dist)

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express()
 app.use(express.json())
 morgan.token('body', function (req, res) {return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
+app.use(express.static('dist'))
 
 let persons = [
   
