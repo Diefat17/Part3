0a0082125a016ec74927191885ae4196b175dee0
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-09 16:33:01 -0600

    Another final commit

--- index.js
+++ index.js
@@ -10,29 +10,26 @@ const app = express()
 //app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body'))
 
 let persons = [
-  [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-
-  ]
+  { 
+    "id": 1,
+    "name": "Arto Hellas", 
+    "number": "040-123456"
+  },
+  { 
+    "id": 2,
+    "name": "Ada Lovelace", 
+    "number": "39-44-5323523"
+  },
+  { 
+    "id": 3,
+    "name": "Dan Abramov", 
+    "number": "12-43-234345"
+  },
+  { 
+    "id": 4,
+    "name": "Mary Poppendieck", 
+    "number": "39-23-6423122"
+  }
 ]
 app.use(express.static('dist'))
 const cors = require('cors')
@@ -50,6 +47,9 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.post('/api/persons', (req, res) => {
+  res.json(req.body)
+})
 
 
 app.get('/info', (req, res) => {
@@ -71,7 +71,7 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.delete('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+  persons = persons.filter(note => note.id !== id)
 
   response.status(204).end()
 })
