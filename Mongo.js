0a0b3df94a71002b2b56e9310011376817fa4aca
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-11 00:12:58 -0600

    Exercise 3.12 done

---
+++ Mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const phone = process.argv[4]
+
+const url =
+  `mongodb+srv://diegordo17:${password}@cluster0.ku17xwi.mongodb.net/personsApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const personsSchema = new mongoose.Schema({
+  name: String,
+  phone: String,
+})
+
+const Persons = mongoose.model('persons', personsSchema)
+
+const person = new Persons({
+    name: name,
+    phone: phone,
+  })
+  
+
+
+person.save().then(result => {
+  console.log('persons saved!')
+  mongoose.connection.close()
+})
+
+/*
+
+persons.find({important: true}).then(result => {
+  result.forEach(persons => {
+    console.log(persons)
+  })
+  mongoose.connection.close()
+})*/
+
+
+
