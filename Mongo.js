bf1b78ecd1b7c2385248ec42d7d719e5644eb99c
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-11 00:23:20 -0600

    The truth done

--- Mongo.js
+++ Mongo.js
@@ -9,6 +9,7 @@ const password = process.argv[2]
 const name = process.argv[3]
 const phone = process.argv[4]
 
+
 const url =
   `mongodb+srv://diegordo17:${password}@cluster0.ku17xwi.mongodb.net/personsApp?retryWrites=true&w=majority`
 
@@ -28,12 +29,23 @@ const person = new Persons({
     phone: phone,
   })
   
+if(!phone && !name){
+    Persons.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(persons => {
+          console.log(persons.name, persons.phone)
+        })
+        mongoose.connection.close()
+      })
+
+} else {
+    person.save().then(result => {
+        console.log(`added ${name} number ${phone} to phonebook`)
+        mongoose.connection.close()
+      })
+}
 
 
-person.save().then(result => {
-  console.log('persons saved!')
-  mongoose.connection.close()
-})
 
 /*
 
