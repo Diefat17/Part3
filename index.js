9dba7de0aa12ba03549730dea743e3804af6c541
Diegordo17 <diegosolorzano608@gmail.com>
2024-05-15 17:26:29 -0600

    Change post

--- index.js
+++ index.js
@@ -82,13 +82,13 @@ app.post('/api/persons', (req, res) => {
     return res.status(400).json({ error: 'content missing' })
   }
 
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
+  const person = new Person({
+    name: body.name,
+    number: body.number,
   })
 
-  note.save().then(savedNote => {
-    res.json(savedNote)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
   })
 })
 
